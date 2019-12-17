cadac1f157b3ba8942c898c692c8053edb51d002
Victor-Chris <victorchris51@gmail.com>
2019-12-17 13:53:44 +0300

    Fixed login issue, added login, signUp, getUsers functions

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -11,17 +11,30 @@ const HttpError = require('../models/http-error');
 //_________MODELS____________
 const User = require('../models/users');
 
-const DUMMY_USERS = [
+/* const DUMMY_USERS = [
     {
         id      : 'u1',
         name    : 'Chris',
         email   : 'chris@abc.com',
         password: 'chris'
     }
-]
+] */
 
-const getUsers = (req, res, next) => {
+/* const getUsers = (req, res, next) => {
     res.json({ users: DUMMY_USERS })
+} */
+
+const getUsers = async (req, res, next) => {
+    let users;
+    try{
+        users = await User.find({}, '-password')
+    }catch(err){
+        const error = new HttpError(
+            'Fetching users failed, please try again later.', 500
+        )
+        return next(error);
+    }
+    res.json({users: users.map(user => user.toObject({ getters: true }))})
 }
 
 /* const signup = (req, res, next) => {
@@ -59,15 +72,13 @@ const signup = async (req, res, next) => {
 
     const { name, email, password, places } = req.body;
 
-    let existingUser
-
+    let existingUser;
     try{
         existingUser = await User.findOne({ email: email });
     }catch(err){
         const error = new HttpError(
             "Signing up failed, please try again later.", 500
         )
-
         return next(error);
     }
 
@@ -78,7 +89,6 @@ const signup = async (req, res, next) => {
         return next(error);
     }
    
-
     const createdUser = new User({
         name,
         email,
@@ -100,12 +110,37 @@ const signup = async (req, res, next) => {
     res.status(201).json({ user: createdUser.toObject({ getters: true }) });
 };
 
-const login = (req, res, next) => {
+/* const login = (req, res, next) => {
     const { email, password } = req.body;
 
     const identifiedUser = DUMMY_USERS.find(u => u.email === email);
     if(!identifiedUser || identifiedUser.password !== password){
         throw new HttpError('Could not identify user', 401);
+    }
+
+    res.json({message: "Logged in"});
+} */
+
+const login = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    let existingUser;
+
+    try{
+        existingUser = await User.findOne({ email: email });
+    }catch(err){
+        const error = new HttpError(
+            "Login in failed, please try again later.", 500
+        )
+
+        return next(error);
+    }
+
+    if(!existingUser || existingUser.password !== password){
+        const error = new HttpError(
+            'Invalid credentials, could not log you in', 401
+        )
+        return next(error);
     }
 
     res.json({message: "Logged in"});
